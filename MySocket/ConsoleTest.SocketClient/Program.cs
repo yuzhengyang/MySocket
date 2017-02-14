dd5b459b1d628957b7e13f7ff1b0ef73b3da36d2
yuzhengyang <inc@live.cn>
2017-02-14 15:57:50 +0800

    Create Project

---
+++ MySocket/ConsoleTest.SocketClient/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ConsoleTest.SocketClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int port = 6000;
+            string host = "127.0.0.1";//服务器端ip地址
+
+            IPAddress ip = IPAddress.Parse(host);
+            IPEndPoint ipe = new IPEndPoint(ip, port);
+
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            clientSocket.Connect(ipe);
+
+            //send message
+            string sendStr = "send to server : hello,ni hao";
+            byte[] sendBytes = Encoding.ASCII.GetBytes(sendStr);
+            clientSocket.Send(sendBytes);
+
+            //receive message
+            string recStr = "";
+            byte[] recBytes = new byte[4096];
+            int bytes = clientSocket.Receive(recBytes, recBytes.Length, 0);
+            recStr += Encoding.ASCII.GetString(recBytes, 0, bytes);
+            Console.WriteLine(recStr);
+
+            clientSocket.Close();
+
+            Console.WriteLine("Press Enter To Exit");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MySocket/ConsoleTest.SocketServer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ConsoleTest.SocketServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int port = 6000;
+            string host = "127.0.0.1";
+
+            IPAddress ip = IPAddress.Parse(host);
+            IPEndPoint ipe = new IPEndPoint(ip, port);
+
+            Socket sSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            sSocket.Bind(ipe);
+            sSocket.Listen(0);
+            Console.WriteLine("监听已经打开，请等待");
+
+            //receive message
+            Socket serverSocket = sSocket.Accept();
+            Console.WriteLine("连接已经建立");
+            string recStr = "";
+            byte[] recByte = new byte[4096];
+            int bytes = serverSocket.Receive(recByte, recByte.Length, 0);
+            recStr += Encoding.ASCII.GetString(recByte, 0, bytes);
+
+            //send message
+            Console.WriteLine("服务器端获得信息:{0}", recStr);
+            string sendStr = "send to client :hello";
+            byte[] sendByte = Encoding.ASCII.GetBytes(sendStr);
+            serverSocket.Send(sendByte, sendByte.Length, 0);
+            serverSocket.Close();
+            sSocket.Close();
+
+            Console.WriteLine("Press Enter To Exit");
+            Console.ReadLine();
+        }
+    }
+}
